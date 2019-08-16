9680bf4908d1d6df9c4b820c6126591ffd929c45
Shantanu Shukla <shantanushukla607@gmail.com>
2019-08-16 19:54:17 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mercury");
+        System.out.println("1) Enter as Merchant");
+        System.out.println("2) Enter as Customer");
+        System.out.println("3) See user details");
+        System.out.println("4) Company account balance");
+        System.out.println("5) Exit");
+    }
+}
